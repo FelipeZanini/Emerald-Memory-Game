4fc4fe9817627f3aad6589bfc83a29ade99359aa
Felipe Silva <felipe_zanini@hotmail.com>
2023-08-18 22:55:27 +0000

    trying to solve bugs

--- assets/scrip/script.js
+++ assets/scrip/script.js
@@ -1,12 +1,11 @@
 let cardSelector = $(".card");
 let cardBackSelector = $(".card-back");
 let IndexSelecteds = [];
-let matches = 0;
+let matches = 6;
 let turns = 0;
 let cardGuesses = [];
 
 function randomizeBoard() {
-
     while (IndexSelecteds.length < 12) {
         let randomNumber = Math.floor(Math.random() * 12);
         if (!IndexSelecteds.includes(randomNumber)) {
@@ -28,36 +27,61 @@ function randomizeBoard() {
 
     setTimeout(() => {
         $(".card-front").on("click", pickBoard);
-        $(".card-container").css("transform","rotateY(0deg)");
+        $(".card-container").css("transform", "rotateY(0deg)");
     },
         200);
 };
 
 function pickBoard() {
-    cardGuesses.push(($(this).siblings(".card-back").get()[0]))   
-    
+    // there is a bug if you double click too fast this if is just ignored
+    if (!cardGuesses.includes(this)) {
+        cardGuesses.push(($(this).siblings(".card-back").get()[0]));
+    }
+    console.log(cardGuesses);
+    $(cardGuesses).parent().removeAttr("style");
+
     if ($(cardGuesses[0]).attr("class") == $(cardGuesses[1]).attr("class")) {
-        // $(`.${cardGuesses[0]}`).css("transform","rotateY(180deg)");
+
         var sucessAudio = new Audio("assets/songs/sucessAudio.wav");
         sucessAudio.play();
         matches++;
         $("#matches").text(`Your Matches: ${matches}`);
         cardGuesses.length = 0;
-    } 
-    
+    }
+
     else if (cardGuesses.length == 2) {
+
+        setTimeout(() => {
+            $(cardGuesses).parent().css("transform", "rotateY(0deg)");
+            cardGuesses.length = 0;
+        },
+            700);
+
         var failAudio = new Audio("assets/songs/failAudio.wav");
         failAudio.play();
         turns++
         $("#turns").text(`Your Turns: ${turns}`);
-        cardGuesses.length = 0;
+
     };
-    
+
     if (matches == 6) {
         $("#matches").text("Congratulations!");
-        $("#turns").text("You won, reload the page to keep playing Im lazy as fuck.");
+        $("#turns").text("You won");
+        $("#restart-button").removeClass("hide");
     };
 };
+
+$("#restart-button").on("click", function(){
+    matches = 0;
+    IndexSelecteds.length = 0;
+    cardGuesses.length = 0;
+    $("#turns").text(`Your Turns: ${turns}`);
+    $("#matches").text(`matches: ${matches}`);
+    $("#restart-button").addClass("hide");
+    $(cardBackSelector).removeClass();
+    $(cardBackSelector).addClass("card-back");
+    randomizeBoard();
+});
 
 
 randomizeBoard();
