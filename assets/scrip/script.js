a95d2791819da86ed0de561706cb8abc1d05123d
Felipe Silva <felipe_zanini@hotmail.com>
2023-08-18 20:32:54 +0000

    this code is so confusing

--- assets/scrip/script.js
+++ assets/scrip/script.js
@@ -1,4 +1,5 @@
 let cardSelector = $(".card");
+let cardBackSelector = $(".card-back");
 let IndexSelecteds = [];
 let matches = 0;
 let turns = 0;
@@ -12,43 +13,46 @@ function randomizeBoard() {
             IndexSelecteds.push(randomNumber);
         };
     };
-    $(cardSelector[IndexSelecteds[0]]).addClass("redCard");
-    $(cardSelector[IndexSelecteds[1]]).addClass("greenCard");
-    $(cardSelector[IndexSelecteds[2]]).addClass("redCard");
-    $(cardSelector[IndexSelecteds[3]]).addClass("purpleCard");
-    $(cardSelector[IndexSelecteds[4]]).addClass("greenCard");
-    $(cardSelector[IndexSelecteds[5]]).addClass("yellowCard");
-    $(cardSelector[IndexSelecteds[6]]).addClass("purpleCard");
-    $(cardSelector[IndexSelecteds[7]]).addClass("whiteCard");
-    $(cardSelector[IndexSelecteds[8]]).addClass("yellowCard");
-    $(cardSelector[IndexSelecteds[9]]).addClass("blackCard");
-    $(cardSelector[IndexSelecteds[10]]).addClass("whiteCard");
-    $(cardSelector[IndexSelecteds[11]]).addClass("blackCard");
+    $(cardBackSelector[IndexSelecteds[0]]).addClass("redCard");
+    $(cardBackSelector[IndexSelecteds[1]]).addClass("greenCard");
+    $(cardBackSelector[IndexSelecteds[2]]).addClass("redCard");
+    $(cardBackSelector[IndexSelecteds[3]]).addClass("purpleCard");
+    $(cardBackSelector[IndexSelecteds[4]]).addClass("greenCard");
+    $(cardBackSelector[IndexSelecteds[5]]).addClass("yellowCard");
+    $(cardBackSelector[IndexSelecteds[6]]).addClass("purpleCard");
+    $(cardBackSelector[IndexSelecteds[7]]).addClass("whiteCard");
+    $(cardBackSelector[IndexSelecteds[8]]).addClass("yellowCard");
+    $(cardBackSelector[IndexSelecteds[9]]).addClass("blackCard");
+    $(cardBackSelector[IndexSelecteds[10]]).addClass("whiteCard");
+    $(cardBackSelector[IndexSelecteds[11]]).addClass("blackCard");
 
     setTimeout(() => {
-        $(cardSelector).css("background-color", "blue");
-        cardSelector.on("click", pickBoard);
+        $(".card-front").on("click", pickBoard);
+        $(".card-container").css("transform","rotateY(0deg)");
     },
-        3500);
+        200);
 };
 
 function pickBoard() {
-    cardGuesses.push(this.className.split(' ')[2]);
-    if (cardGuesses[0] == cardGuesses[1]) {
-        $(`.${cardGuesses[0]}`).removeAttr("style");
-        $(`.${cardGuesses[0]}`).off();
+    cardGuesses.push(($(this).siblings(".card-back").get()[0]))   
+    
+    if ($(cardGuesses[0]).attr("class") == $(cardGuesses[1]).attr("class")) {
+        // $(`.${cardGuesses[0]}`).css("transform","rotateY(180deg)");
         var sucessAudio = new Audio("assets/songs/sucessAudio.wav");
         sucessAudio.play();
         matches++;
         $("#matches").text(`Your Matches: ${matches}`);
         cardGuesses.length = 0;
-    } else if (cardGuesses.length == 2) {
+    } 
+    
+    else if (cardGuesses.length == 2) {
         var failAudio = new Audio("assets/songs/failAudio.wav");
         failAudio.play();
         turns++
         $("#turns").text(`Your Turns: ${turns}`);
         cardGuesses.length = 0;
     };
+    
     if (matches == 6) {
         $("#matches").text("Congratulations!");
         $("#turns").text("You won, reload the page to keep playing Im lazy as fuck.");
