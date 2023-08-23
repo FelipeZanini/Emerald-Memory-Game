80a152e9536972fbf5b2e23e6dcdb4c7666e1ccc
Felipe Silva <felipe_zanini@hotmail.com>
2023-08-23 21:54:28 +0000

    babaca

--- assets/scrip/script.js
+++ assets/scrip/script.js
@@ -1,27 +1,16 @@
 $(document).ready(function () {
-    // function main() {
+    // Global variables 
     let cardSelector = $(".card");
     let cardBackSelector = $(".card-back");
     let IndexSelecteds = [];
     let cardGuesses = [];
-    let matches = 6;
+    let matches = 0;
     let turns = 0;
-    
+
+    // Randomize the positons of the cards on the board
     randomizeBoard();
-
-    
-    $("#start-button").on("click", function () {
-        setTimeout(() => {
-            $(".card-front").on("click", pickBoard);
-            $("#start-button").addClass("hide");
-            $(".card-container").css("transform", "rotateY(180deg)");
-            setTimeout(() => $(".card-container").css("transform", "rotateY(0deg)"), 1000);
-        },
-            200);
-    });
-
     function randomizeBoard() {
-
+        // Get a array with number from 0 to 11, in a random sequence
         while (IndexSelecteds.length < 12) {
             let randomNumber = Math.floor(Math.random() * 12);
             if (!IndexSelecteds.includes(randomNumber)) {
@@ -29,6 +18,7 @@ $(document).ready(function () {
             };
         };
 
+        // Add card classes in a random ordem
         $(cardBackSelector[IndexSelecteds[0]]).addClass("redCard");
         $(cardBackSelector[IndexSelecteds[1]]).addClass("greenCard");
         $(cardBackSelector[IndexSelecteds[2]]).addClass("redCard");
@@ -44,18 +34,19 @@ $(document).ready(function () {
 
     };
 
-
+    // Function called when a click event is fired on a card
     function pickBoard() {
-        // there is a bug if you double click too fast this if is just ignored
-        console.log(this);
+
+        // Block of code to assure the user is guessing a new card
         if (!cardGuesses.includes($(this).siblings(".card-back").get()[0])) {
             cardGuesses.push(($(this).siblings(".card-back").get()[0]));
         }
 
+        // Flip the selected card
         $(cardGuesses).parent().css("transform", "rotateY(180deg)");
 
+        // Block of code to check if the user choose the right combinations of cards
         if ($(cardGuesses[0]).attr("class") == $(cardGuesses[1]).attr("class")) {
-
             var sucessAudio = new Audio("assets/songs/sucessAudio.wav");
             sucessAudio.play();
             matches++;
@@ -63,49 +54,58 @@ $(document).ready(function () {
             cardGuesses.length = 0;
         }
 
+        // Block of code to check if the user choose the wrong combinations of cards
         else if (cardGuesses.length == 2) {
-
-            setTimeout(() => {
-                $(cardGuesses).parent().css("transform", "rotateY(0deg)");
-                cardGuesses.length = 0;
-            },
-                800);
 
             var failAudio = new Audio("assets/songs/failAudio.wav");
             failAudio.play();
+            setTimeout(() => {
+                $(cardGuesses).parent().css("transform", "rotateY(0deg)");
+                cardGuesses.length = 0;
+            }, 800);
+
             turns++
             $("#turns").text(`Turns: ${turns}`);
 
         };
 
+        // Chunk of code to check if the game is over
         if (matches == 6) {
+            $(".card-front").off();
             $("#matches").text("Congrats!");
             $("#turns").text("You win");
             $("#restart-button").removeClass("hide");
+            $(".card-container").addClass("blur")
         };
     };
 
+    // Restart button, reset all the variables to the innital value and run the game again
     $("#restart-button").on("click", function () {
-        $(".card-front").off();
+
+        $(".card-container").removeClass("blur")
+        $("#restart-button").addClass("hide");
         matches = 0;
         turns = 0;
         IndexSelecteds.length = 0;
         cardGuesses.length = 0;
         $("#turns").text(`Turns:`);
         $("#matches").text(`Matches:`);
-        $("#restart-button").addClass("hide");
         $(cardBackSelector).removeClass();
         $(cardBackSelector).addClass("card-back");
         randomizeBoard();
+        $(".card-container").css("transform", "rotateY(180deg)");
+        setTimeout(() => $(".card-container").css("transform", "rotateY(0deg)"), 1500);
+        $(".card-front").on("click", pickBoard);
 
-        setTimeout(() => {
-            $(".card-front").on("click", pickBoard);
-            $("#restart-button").addClass("hide");
-            $(".card-container").css("transform", "rotateY(180deg)");
-            setTimeout(() => $(".card-container").css("transform", "rotateY(0deg)"), 1000);
-        },
-            200);
-    });
     });
 
+    // Start button
+    $("#start-button").on("click", function () {
 
+        $("#start-button").addClass("hide");
+        $(".card-container").css("transform", "rotateY(180deg)");
+        setTimeout(() => $(".card-container").css("transform", "rotateY(0deg)"), 1500);
+        $(".card-front").on("click", pickBoard);
+    });
+
+});
