6e2f92f7f10daffd8ec7ba124ea0038791820b3f
Felipe Silva <felipe_zanini@hotmail.com>
2023-08-24 21:05:08 +0000

    xablau

--- assets/scrip/script.js
+++ assets/scrip/script.js
@@ -36,48 +36,50 @@ $(document).ready(function () {
 
     // Function called when a click event is fired on a card
     function pickBoard() {
-        
+
         // Block of code to assure the user is guessing a new card
-        if (!cardGuesses.includes($(this).siblings(".card-back").get()[0])) {
+        if (!cardGuesses.includes($(this).siblings(".card-back").get()[0]) && cardGuesses.length < 2) {
             cardGuesses.push(($(this).siblings(".card-back").get()[0]));
-        }
+            console.log(this);
+            console.log(cardGuesses);
+            // Flip the selected card
+            $(cardGuesses).parent().css("transform", "rotateY(180deg)");
 
-        // Flip the selected card
-        $(cardGuesses).parent().css("transform", "rotateY(180deg)");
-
-        // Block of code to check if the user choose the right combinations of cards
-        if ($(cardGuesses[0]).attr("class") == $(cardGuesses[1]).attr("class")) {
-            var sucessAudio = new Audio("assets/songs/sucessAudio.wav");
-            sucessAudio.play();
-            matches++;
-            $("#matches").text(`Matches: ${matches}`);
-            cardGuesses.length = 0;
-        }
-
-        // Block of code to check if the user choose the wrong combinations of cards
-        else if (cardGuesses.length == 2) {
-
-            var failAudio = new Audio("assets/songs/failAudio.wav");
-            failAudio.play();
-            setTimeout(() => {
-                $(cardGuesses).parent().css("transform", "rotateY(0deg)");
+            // Block of code to check if the user choose the right combinations of cards
+            if ($(cardGuesses[0]).attr("class") == $(cardGuesses[1]).attr("class")) {
+                var sucessAudio = new Audio("assets/songs/sucessAudio.wav");
+                sucessAudio.play();
+                matches++;
+                $("#matches").text(`Matches: ${matches}`);
                 cardGuesses.length = 0;
-            }, 800);
+            }
 
-            turns++
-            $("#turns").text(`Turns: ${turns}`);
+            // Block of code to check if the user choose the wrong combinations of cards
+            else if (cardGuesses.length == 2) {
 
-        };
+                var failAudio = new Audio("assets/songs/failAudio.wav");
+                failAudio.play();
+                setTimeout(() => {
+                    $(cardGuesses).parent().css("transform", "rotateY(0deg)");
+                    cardGuesses.length = 0;
+                }, 800);
 
-        // Chunk of code to check if the game is over
-        if (matches == 6) {
-            $(".card-front").off();
-            $("#matches").text("Congrats!");
-            $("#turns").text("You win");
-            $("#restart-button").removeClass("hide");
-            $(".card-container").addClass("blur")
-        };
+                turns++
+                $("#turns").text(`Turns: ${turns}`);
+
+            };
+
+            // Chunk of code to check if the game is over
+            if (matches == 6) {
+                $(".card-front").off();
+                $("#matches").text("Congrats!");
+                $("#turns").text("You win");
+                $("#restart-button").removeClass("hide");
+                $(".card-container").addClass("blur")
+            };
+        }
     };
+
 
     // Restart button, reset all the variables to the innital value and run the game again
     $("#restart-button").on("click", function () {
